5e34b73ffda894168f0498a1215e2e45736009b4
5wapnill <mswapnil383@gmail.com>
2025-01-17 10:12:23 +0530

    Done

---
+++ 1stAssignments/5thAssignment.aspx.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace _1stAssignments
+{
+    public partial class WebForm3 : System.Web.UI.Page
+    {
+        protected void Button5_Click(object sender, EventArgs e)
+        {
+            here.Visible = true;
+            here.Text += ListBox.SelectedItem.Value;
+        }
+    }
+}
